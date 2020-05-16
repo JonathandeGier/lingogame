2bc1981e9541cc78845286b51d4fbe70d8294e65
Jonathan <jonathan.degier@student.hu.nl>
2020-05-16 11:54:13 +0200

    init commit

---
+++ src/main/java/nl/jonathandegier/lingogame/LingoGame.java
@@ -0,0 +1,11 @@
+package nl.jonathandegier.lingogame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LingoGame {
+    public static void main(String[] args) {
+        SpringApplication.run(LingoGame.class, args);
+    }
+}
