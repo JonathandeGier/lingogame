ec37f2fd109c60203b96c9d848aeb00730e68265
Jonathan <jonathan.degier@student.hu.nl>
2020-05-23 23:00:01 +0200

    rework game.js

--- src/main/resources/static/webapp/js/game.js
+++ src/main/resources/static/webapp/js/game.js
@@ -1,23 +1,6 @@
-var feedbacks = [
-    {
-        guess: "h",
-        wordLength: 5,
-        totalGuesses: 5,
-        guessesLeft: 5
-    },
-    {
-        guess: "hello",
-        wordLength: 5,
-        totalGuesses: 5,
-        guessesLeft: 4
-    },
-    {
-        guess: "helloas",
-        wordLength: 5,
-        totalGuesses: 5,
-        guessesLeft: 3
-    }
-];
+var initialFeedback;
+
+var givenFeedbacks;
 
 document.getElementById("guessInput").addEventListener("keypress", function (ev) {
     if (ev.key === "Enter") {
@@ -28,7 +11,41 @@ document.getElementById("guessInput").addEventListener("keypress", function (ev)
 function init() {
     // TODO: api request to start a new game
 
+    buildEmptyTable(5, 5);
+
+
+}
+
+function startRound() {
+    // TODO: api request to start a new round and fill initialFeedback and empty givenFeedbacks
+
+    initialFeedback = {
+        guess: "w",
+        feedback: [
+            {
+                letter: "w",
+                feedbackType: "CORRECT"
+            }
+        ],
+        wordLength: 5,
+        totalGuesses: 5,
+        guessesLeft: 5
+    };
+
+    givenFeedbacks = [];
+
     buildTable();
+}
+
+function guess() {
+    var input = document.getElementById("guessInput");
+    var guess = input.value;
+
+    // TODO: api request to guess
+
+    givenFeedbacks.push({guess: guess});
+    buildTable()
+    input.value = "";
 }
 
 function buildTable() {
@@ -39,24 +56,50 @@ function buildTable() {
         table.removeChild(table.firstChild);
     }
 
-    for (i = 0; i < feedbacks[0].totalGuesses; i++) {
+    for (i = 0; i < initialFeedback.totalGuesses; i++) {
         var row = table.insertRow(-1);
 
-        for (j = 0; j < feedbacks[0].wordLength; j++) {
+        for (j = 0; j < initialFeedback.wordLength; j++) {
             cell = row.insertCell();
             cell.classList.add("lingo-cell");
 
-            if (feedbacks[i] !== undefined) {
-                cell.innerHTML = feedbacks[i].guess.charAt(j).toUpperCase();
+            if (givenFeedbacks[i] !== undefined) {
+                // feedback for this guess has been given, so display it
+                cell.innerHTML = givenFeedbacks[i].guess.charAt(j).toUpperCase();
+                if (givenFeedbacks[i].feedback[j] !== undefined) {
+                    if (givenFeedbacks[i].feedback[j].feedbackType === "CORRECT") {
+                        cell.classList.add("correct")
+                    } else if (givenFeedbacks[i].feedback[j].feedbackType === "PRESENT") {
+                        cell.classList.add("present")
+                    }
+                }
+            } else {
+                if (i === 0 && initialFeedback.feedback[j] !== undefined && initialFeedback.feedback[j].feedbackType === "CORRECT") {
+                    // no given feedback on the first guess means the game has just started, so display the initial feedback
+                    cell.innerHTML = initialFeedback.guess.charAt(j).toUpperCase();
+                } else if (givenFeedbacks[i - 1] !== undefined && givenFeedbacks[i - 1].feedback[j] !== undefined && givenFeedbacks[i - 1].feedback[j].feedbackType === "CORRECT") {
+                    // display correct letters of last feedback
+                    cell.innerHTML = givenFeedbacks[i - 1].guess.charAt(j).toUpperCase();
+                }
             }
         }
     }
 }
 
-function guess() {
-    var input = document.getElementById("guessInput");
-    var guess = input.value;
-    feedbacks.push({guess: guess});
-    buildTable()
-    input.value = "";
+function buildEmptyTable(wordLength, guesses) {
+    var table = document.getElementById("game");
+
+    // clear the table
+    while (table.hasChildNodes()) {
+        table.removeChild(table.firstChild);
+    }
+
+    for (i = 0; i < guesses; i++) {
+        var row = table.insertRow(-1);
+
+        for (j = 0; j < wordLength; j++) {
+            cell = row.insertCell();
+            cell.classList.add("lingo-cell");
+        }
+    }
 }
